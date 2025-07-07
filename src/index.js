06fee3c905085073b9241a1c7f97a9d506d65f4b
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-07-07 06:32:26 +0200

    plan out proj

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./styles.css"
+
+
+
+
+
+
+
+
+
+
+
+
+
+//projects factory
+    // - default Project
+    // - create new Projects
+
+//projectsManager
+    // - get / - add / - remove projects
+    // - helper functions
+
+//project
+    // - keep track of todo-items
+    // - title
+    // - color
+
+//todo-item
+    // - title
+    // - description
+    // - dueDate
+    // - prio
+    // - checked
+
+
+
+
+//dom Stuff
+    // - render projects in list
+    // - make new screen with todo-items
+    // - render todo-items
+    // - todo on click create pop-up
+
+// index
+    // -- button logic in a module
